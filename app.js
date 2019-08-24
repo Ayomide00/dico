579d6104fd8836c2af25ee6b9c807bee81bd895c
Qudusayo <zeekimono@gmail.com>
2019-08-24 02:19:13 +0100

    updating

--- app.js
+++ app.js
@@ -4,50 +4,57 @@ document.querySelector('#search').addEventListener('click', function (e) {
 
     document.querySelector('#top').innerHTML = word;
 
-    fetch('kill.json')
+    fetch('love.json')
     .then(function (res){
         return res.json()
     })
     .then(function (data){
         data.forEach(item => {
-            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray"><h4>${item.meta.id}</h4></p>`
+            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125"><b><h4>${item.meta.id}</h4></b></p>`
     
-            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray"><b>${item.fl}</b></p>`
+            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125"><b>${item.fl}</b></p>`
 
 
 
             console.log(item.meta.id)
             console.log(item.fl)
             item.shortdef.forEach(meanings => {
-                document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">${meanings}</p>`
+                document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125">${meanings}</p>`
 
                 console.log(meanings);
             });
-            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray"><b><h4>Synonymns</h4></b></p>`
+            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125"><b><h5>Synonymns</h5></b></p>`
 
+            let synos = []
             item.meta.syns.forEach(test => {
                 test.forEach(syno => {
-                    document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">${syno}</p>`
-
+                    // document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125">${syno}</p>`
+                    synos.push(` ${syno}`);
                     console.log(syno)
                 })
+                console.log(synos.toString())
 
 
             })
-            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray"><b><h4>Antonymns</h4></b</p>`
+            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125">${synos}</p>`
+
+            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125"><b><h5>Antonymns</h5></b></p>`
+            let antos = '';
             item.meta.ants.forEach(test => {
                 test.forEach(anto => {
-                    document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">${anto}</p>`
-
+                    // document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125">${anto}</p>`
+                    antos += ` ${anto}`
                     console.log(anto)
                 })
             })
+            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125">${antos}</p>`
+
             // item.def[0].sseq.forEach(terms => {
             //     terms[0][1].syn_list[0].forEach(synonyms => {
             //         console.log(synonyms.wd)
             //     });
             // });
-            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">--------------------------------------</p>`
+            document.querySelector('#meaning').innerHTML += `<p class="media-body pb-3 mb-0 small lh-125">--------------------------------------</p>`
 
             console.log('--------------------------------------')
     
